d41c5f4255e528351e5fa997577b344cfd181cc3
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2023-11-02 01:16:32 +0700

    initial commit, setup flutterfire

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_app/firebase_options.dart';
+
+import 'widget_tree.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const WidgetTree(),
+    );
+  }
+}
